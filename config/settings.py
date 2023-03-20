0e5a1465034c4a2f7647a14a8a8f54bcaa8e20eb
tchappui <tchappui@gmail.com>
2023-03-20 10:47:45 +0100

    Fixed some config mistakes

--- config/settings.py
+++ config/settings.py
@@ -16,26 +16,7 @@ import environ
 import sentry_sdk
 
 env = environ.Env()
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
-# sentry_sdk.init(
-#     dsn="https://c1e4b46eb53e40acba4401ca46a9e171@o4504820571832320.ingest.sentry.io/4504820575830016",
-#     integrations=[
-#         DjangoIntegration(),
-#     ],
-#     traces_sample_rate=1.0,
-#    send_default_pii=True
-# )
-
-sentry_sdk.init(
-    dsn=env("SENTRY_DSN"),
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
-    traces_sample_rate=1.0,
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +31,6 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
-# DEBUG = True
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 # ALLOWED_HOSTS = []
@@ -154,3 +134,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configuration of sentry
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=env("SENTRY_DSN"),
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
+        traces_sample_rate=1.0,
+    )
