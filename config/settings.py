1568c5366fb920b91f301518e2317e568d8b4e38
BernicheAurelie <berniche.aurelie@orange.fr>
2023-12-07 16:56:23 +0100

    add sentry settings and modify image in detail recipe template

--- config/settings.py
+++ config/settings.py
@@ -186,12 +186,13 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# if not DEBUG:
-#     sentry_sdk.init(
-#         dsn=env("SENTRY_DSN"),
-
-#         # Set traces_sample_rate to 1.0 to capture 100%
-#         # of transactions for performance monitoring.
-#         # We recommend adjusting this value in production,
-#         traces_sample_rate=1.0,
-#     )
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
